84b2e81c38254a5311761e805f65615e08a65abe
FernanMoreno <fernandojosemolinamore@gmail.com>
2026-07-14 18:30:24 +0200

    Harden runtime bone registry

---
+++ Core/Tests/Runtime/RagdollDefinitionBindingsPlayModeTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Hairibar.Ragdoll.Tests
+{
+    public class RagdollDefinitionBindingsPlayModeTests
+    {
+        [UnityTest]
+        public IEnumerator RegistryLookups_WorkAfterEnteringPlayMode()
+        {
+            using (RagdollBindingsTestRig rig = new RagdollBindingsTestRig())
+            {
+                yield return null;
+
+                RagdollBoneHandle handle;
+                Assert.That(rig.Bindings.TryGetBoneHandle(rig.ChildBody, out handle), Is.True);
+                Assert.That(rig.Bindings.GetBone(handle).Name, Is.EqualTo(rig.ChildName));
+                Assert.That(rig.Bindings.TryGetBoneHandle(rig.ChildCollider, out handle), Is.True);
+                Assert.That(rig.Bindings.GetBone(handle).Name, Is.EqualTo(rig.ChildName));
+            }
+        }
+    }
+}
